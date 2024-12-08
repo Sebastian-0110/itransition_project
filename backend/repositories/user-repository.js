dfae8c098077e20193f00323946d5d8c9fcf21c4
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2024-12-08 17:32:01 +0000

    Implement findByEmail & use the mapper for the returns

--- backend/repositories/user-repository.js
+++ backend/repositories/user-repository.js
@@ -1,14 +1,21 @@
+const UserMapper = require('./mappers/user-mapper');
+
+
 class UserRepository {
     constructor(userModel) {
         this.userModel = userModel;
     }
 
     async findById(id) {
-        return await this.userModel.findByPk(id);
+        return UserMapper.toDTO(await this.userModel.findByPk(id));
+    }
+
+    async findByEmail(email) {
+        return UserMapper.toDTO(await this.userModel.findOne({ where: { email } }));
     }
 
     async create(data) {
-        return await this.userModel.create(data);
+        return UserMapper.toDTO(await this.userModel.create(data));
     }
 }
 
