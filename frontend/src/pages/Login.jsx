41990310b30994a620b35159e2609ebef2da1239
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2024-12-24 08:05:11 +0000

    Add login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import { Link } from "react-router";
+
+function Login() {
+	return (
+		<Container fluid="md" className="p-3">
+			<Row className="justify-content-center align-items-center">
+				<Col md={6}>
+					<Form className="d-flex flex-column gap-4 justifiy-content-center">
+						<Form.Group controlId="email">
+							<Form.Label>Email</Form.Label>
+							<Form.Control type="email" placeholder="john-doe@gmail.com" />
+						</Form.Group>
+
+						<Form.Group controlId="password">
+							<Form.Label>Password</Form.Label>
+							<Form.Control type="password" placeholder="qwerty123" />
+						</Form.Group>
+
+						<Button type="submit" variant="primary">Log in</Button>
+
+						<Form.Text as="p" className="text-center">
+							Don't have an account? <Link to="/auth/signup">Sign up!</Link>
+						</Form.Text>
+					</Form>
+				</Col>
+			</Row>
+
+		</Container>
+	)
+}
+
+export default Login;
