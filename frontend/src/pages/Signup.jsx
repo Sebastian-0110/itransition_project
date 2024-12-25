9660fbc517d28e02526c90d665b22636bbbe0cdf
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2024-12-25 03:43:25 +0000

    Add sign up page

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,45 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import {Link} from "react-router";
+
+function Signup() {
+	return (
+		<Container fluid="md" className="p-3 h-100 align-items-center">
+			<Row className="h-100 justify-content-center align-items-center">
+				<Col md={6}>
+					<Form className="d-flex flex-column gap-4 justifiy-content-center">
+						<h1 className="mx-auto mb-3">Sign up</h1>
+
+						<Form.Group controlId="username">
+							<Form.Label>Usernme</Form.Label>
+							<Form.Control type="text" placeholder="john doe" />
+						</Form.Group>
+
+						<Form.Group controlId="email">
+							<Form.Label>Email</Form.Label>
+							<Form.Control type="email" placeholder="john-doe@gmail.com" />
+						</Form.Group>
+
+						<Form.Group controlId="password">
+							<Form.Label>Password</Form.Label>
+							<Form.Control type="password" placeholder="qwerty123" />
+						</Form.Group>
+
+						<Button type="submit" variant="primary">Sign up</Button>
+
+						<Form.Text as="p" className="text-center">
+							Don't have an account? <Link to="/auth/signup">Sign up!</Link>
+						</Form.Text>
+					</Form>
+				</Col>
+			</Row>
+
+		</Container>
+	)
+
+}
+
+export default Signup;
