2181deed66e11d9aee92d2e3439baee39003314d
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2024-12-27 12:20:49 +0000

    Add morgan for request logging in development

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
 const { passport, session } = require("./middleware");
 const authRoutes = require("./routes/auth");
+require("dotenv").config();
 
 
 const app = express();
 const port = 5000;
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
