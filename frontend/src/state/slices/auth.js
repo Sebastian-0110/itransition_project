70c0b8c64e159ebff905b5b408f95b91b71299cb
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2024-12-31 15:25:11 +0000

    Add support for loading auth state

--- frontend/src/state/slices/auth.js
+++ frontend/src/state/slices/auth.js
@@ -4,6 +4,7 @@ import { fetchAuthState } from "../thunks/auth.js";
 const initialState = {
 	isLoggedIn: false,
 	user: null,
+	isDoneLoading: null,
 }
 
 const authSlice = createSlice({
@@ -19,9 +20,18 @@ const authSlice = createSlice({
 		},
 	},
 	extraReducers: (builder) => {
+		builder.addCase(fetchAuthState.pending, (state) => {
+			state.isDoneLoading = false;
+		});
+
 		builder.addCase(fetchAuthState.fulfilled, (state, action) => {
 			state.isLoggedIn = true;
 			state.user = action.payload;
+			state.isDoneLoading = true;
+		});
+
+		builder.addCase(fetchAuthState.rejected, (state) => {
+			state.isDoneLoading = true;
 		});
 	},
 });
@@ -31,3 +41,4 @@ export const { setLoggedIn, setUser } = authSlice.actions;
 
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 export const selectUser = (state) => state.auth.user;
+export const selectIsDoneLoading = (state) => state.auth.isDoneLoading;
