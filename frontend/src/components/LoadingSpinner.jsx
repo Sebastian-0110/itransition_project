5f35abc1c55a0d91885627ace180ff0daed10abd
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-01-01 14:50:16 +0000

    Replace react-spinners with the react-bootstrap spinners

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -1,10 +1,10 @@
 import Container from "react-bootstrap/Container";
-import { MoonLoader } from "react-spinners";
+import Spinner from 'react-bootstrap/Spinner';
 
 function LoadingSpinner() {
 	return (
 		<Container className="vw-100 vh-100 d-flex justify-content-center align-items-center flex-column">
-			<MoonLoader color="#0d6efd" size={50} loading />
+			<Spinner animation="border" variant="primary" />
 		</Container>
 	)
 }
