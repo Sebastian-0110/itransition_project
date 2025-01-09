ccfecd65cc437affa0a64b658eb98eb748d79a4f
Sebastian Mendoza <sebastian.mendoza.clases@gmail.com>
2025-01-09 08:32:29 +0000

    Add Error 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,9 @@ function App() {
 				<Route path="users" element={<ManageUsers />} />
 			</Route>
 
-			<Route path="*" element={<NotFound/>}/>
+			<Route element={<LandingLayout />} >
+				<Route path="*" element={<NotFound/>} />
+			</Route>
 		</Routes>
 	)
 }
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,6 +1,20 @@
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router";
+
 function NotFound() {
 	return (
-		<p>Not found page</p>
+		<Container className="pt-5 mt-5 d-flex flex-column justify-content-center align-items-center text-center">
+			<h2 className="h1">Error 404</h2>
+
+			<p>
+				The page you requested <span className="text-decoration-underline">wasn't found.</span>
+			</p>
+
+			<Link to="/" className="pt-4">
+				<Button>Go back</Button>
+			</Link>
+		</Container>
 	)
 }
 
